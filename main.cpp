d5a3c323ac63be22d7994230beabe4958dff3d4d
Salvatore Bernasconi <salvatore.bernasconi@hotmail.com>
2024-01-11 15:56:13 +0000

    palle

--- main.cpp
+++ main.cpp
@@ -3,31 +3,56 @@
 
 using namespace std;
 
-char** guests()
+class partyGuests
+{
+  public:
+    string name;
+    int number;
+    
+}
+
+char** guests(int* returnSize)
 {
   int namesSize;
   
   cout << "Insert qty" << endl;
   cin >> namesSize;
-  
-  char** list = new char*[namesSize];
 
-  cout << ""
+  *returnSize = namesSize;        
   
-  for (int i = 0; i < namesSize; i++)
+  char** list = new char*[namesSize];     // dynamically allocated array of pointers
+
+  cout << "1. to start the for" << endl;
+  int select;
+  cin >> select;
+
+  if (select == 1)
   {
-    cout << "Insert Names: " << endl;
-    cin >> list[i];
+    for (int i = 0; i < namesSize; i++)
+    {
+      list[i] = new char[15];
+      
+      cout << "Insert Names: " << endl;
+      cin >> list[i];
+    }
   }
-}
 
+  return list;
+}
 
 int main(void) 
 {
 
-  
-  
+  int size;
+  char** listona = guests(&size);
 
-  
+
+  cout << "Names:" << endl;
+  for (int i = 0; i < 5; i++)
+  { 
+    cout << listona[i] << endl;
+    delete listona[i];
+  }
+  delete listona;
   return 0;
 }
